825405c5bfc0ecd7ee6b4a43114ec4cd2e8ef84c
LinsLeonardo <linsleonardo22@gmail.com>
2021-08-09 15:52:59 -0300

    final

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -1,12 +1,27 @@
 import pickle
 from fastapi import FastAPI
+import pandas as pd
 
 app = FastAPI()
 @app.post('/model')
-## Coloque seu codigo na função abaixo
-def titanic(Sex:int):
-    with open('model/Titanic.pkl', 'rb') as fid: 
+
+def titanic(Sex:int, Age: float, Lifeboat: int, Pclass: int):
+    dados = pd.DataFrame([[Sex,Age, Lifeboat,Pclass]], columns = ['Sex','Age','Lifeboat','Pclass'])
+    with open('codigo/model/Titanic.pkl', 'rb') as fid: 
         titanic = pickle.load(fid)
+
+    resposta = titanic.predict(dados)
+
+    if resposta[0] == 1:
+        survived = True
+        message = "Sobreviveu!!!"
+    else: 
+        survived = False
+        message = "Não sobreviveu :/"
+    
+    retornar = {'survived': survived, 'status':200, 'message': message}
+
+    return retornar
 
 @app.get('/model')
 def get():
